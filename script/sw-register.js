e01747ac90a365bde5220d06424f1cc15199aad2
Noorudheen Yousuf <noorudheenmuzammil09@gmail.com>
2026-08-05 12:02:51 +0530

    delete unwanted files

--- script/sw-register.js
+++
@@ -1,8 +0,0 @@
-// Register service worker if supported
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('Service worker registered.', reg))
-      .catch(err => console.warn('Service worker registration failed:', err));
-  });
-}
